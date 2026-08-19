ddef74b87b810bb06bd14d24e4800e5d41371cb2
Obioma Onwuka <obioma_onwuka@protonmail.com>
2026-08-19 13:41:16 +0100

    First Java practical: Mad Libs game

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,29 @@ public class Main
         String verb1;
         String adjective3;
 
-        System.out.print("Enter an adjective (hint: a word describing a noun):");
+        System.out.print("Enter an adjective (hint: a word describing a noun): ");
         adjective1 = scanner.nextLine();
 
         System.out.print("Enter a noun (hint: animal or person): ");
         noun1 = scanner.nextLine();
 
+        System.out.print("Enter an adjective (hint: a word describing a noun): ");
+        adjective2 = scanner.nextLine();
+
+        System.out.print("Enter a verb (an action word ending with -ing-): ");
+        verb1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective (hint: a word describing a noun): ");
+        adjective3 = scanner.nextLine();
+
+        /*
+        Output the full story.
+        Add the variables into the story
+         */
+
+        System.out.println("\nI went to the market this morning and I saw an " + adjective1 + " " + noun1 + ".");
+        System.out.println("The " + noun1 + " was not " + adjective2 + ".");
+        System.out.println(verb1 + " has become a challenge. The economy is not stable." + "The " + adjective3 + " " + noun1 + " highlighted.");
 
         scanner.close();
     }
